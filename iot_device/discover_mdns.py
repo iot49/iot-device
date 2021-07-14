ed5706acfd6fa0bdb1e84db02bb6babf5e6c0d0a
Bernhard Boser <bernhard.e.boser@gmail.com>
2021-07-13 17:36:53 -0700

    disable mdns discovery (task error)

--- iot_device/discover_mdns.py
+++ iot_device/discover_mdns.py
@@ -16,18 +16,20 @@ class DiscoverMdns(Discover):
     def scan(self):
         # return url's of devices that are online
         self.urls = set()
-        zc = Zeroconf()
-        try:
-            ServiceBrowser(zc, "_mp._tcp.local.", self)
-            ServiceBrowser(zc, "_ws._tcp.local.", self)
-            ServiceBrowser(zc, "_telnet._tcp.local.", self)
-            # give the scanner some time to find everything ...
-            time.sleep(1)
-        except Exception as ex:
-            print(ex)
-            logger.exception(f"Unhandled exception in DiscoverMdns._scanner")
-        finally:
-            zc.close()
+        if False:
+            # asyncio ... task exception error (see below)
+            zc = Zeroconf()
+            try:
+                ServiceBrowser(zc, "_mp._tcp.local.", self)
+                ServiceBrowser(zc, "_ws._tcp.local.", self)
+                ServiceBrowser(zc, "_telnet._tcp.local.", self)
+                # give the scanner some time to find everything ...
+                time.sleep(1)
+            except Exception as ex:
+                print(ex)
+                logger.exception(f"Unhandled exception in DiscoverMdns._scanner")
+            finally:
+                zc.close()
         return self.urls
 
     def _url(self, zeroconf, service_type, name):
@@ -47,3 +49,17 @@ class DiscoverMdns(Discover):
 
     def remove_service(self, zeroconf, service_type, name):
         pass
+
+
+"""
+13.07.21 17:02:20 (-0700)  jupyter  ERROR:asyncio:Task exception was never retrieved
+13.07.21 17:02:20 (-0700)  jupyter  future: <Task finished name='Task-1' coro=<AsyncEngine._async_setup() done, defined at /usr/local/lib/python3.8/site-packages/zeroconf/_core.py:104> exception=OSError(9, 'Bad file descriptor')>
+13.07.21 17:02:20 (-0700)  jupyter  Traceback (most recent call last):
+13.07.21 17:02:20 (-0700)  jupyter    File "/usr/local/lib/python3.8/site-packages/zeroconf/_core.py", line 107, in _async_setup
+13.07.21 17:02:20 (-0700)  jupyter      await self._async_create_endpoints()
+13.07.21 17:02:20 (-0700)  jupyter    File "/usr/local/lib/python3.8/site-packages/zeroconf/_core.py", line 133, in _async_create_endpoints
+13.07.21 17:02:20 (-0700)  jupyter      transport, protocol = await loop.create_datagram_endpoint(lambda: AsyncListener(self.zc), sock=s)
+13.07.21 17:02:20 (-0700)  jupyter    File "/usr/local/lib/python3.8/asyncio/base_events.py", line 1231, in create_datagram_endpoint
+13.07.21 17:02:20 (-0700)  jupyter      sock.setblocking(False)
+13.07.21 17:02:20 (-0700)  jupyter  OSError: [Errno 9] Bad file descriptor
+"""
--- iot_device/version.py
+++ iot_device/version.py
@@ -1,3 +1,3 @@
-version_info = (0, 5, 9)
+version_info = (0, 5, 10)
 
 __version__ = '.'.join(map(str, version_info))
